f0f4408156eb71ca020520e9a3d44e9bcf73e1c0
mLordswe <alexander.csore@gmail.com>
2024-09-04 15:14:13 +0200

    added image

--- public/app.js
+++ public/app.js
@@ -1,2 +1,17 @@
 console.log('Hej')
-console.timeLog()
+let x = 12;
+console.log(x)
+console.log(typeof x)
+x = "Hej"
+console.log(x)
+console.log(typeof x)
+const myName = "alex"
+// console.log("Hello" + " " + n );
+console.log(`Detta är en sträng ${myName} `)
+let age = 25
+const hobby = "Spela spel"
+
+
+console.log(`Hej jag heter ${myName} och jag är ${age} år gammal. Min hobby är att ${hobby}`)
+
+
